dca75fe292dfc4d56dbba30b21d0501b2102355a
mxwll <mxwllndrsn@gmail.com>
2024-01-14 01:17:44 -0800

    huggingface configuration

---
+++ hf_conf.py
@@ -0,0 +1,24 @@
+import json
+from sagemaker.huggingface import HuggingFaceModel
+
+# sm conf
+instance_type = "ml.g5.48xlarge"
+number_of_gpu = 8
+health_check_timeout = 300
+
+# model/endpoint conf
+config = {
+    'HF_MODEL_ID': "mistralai/Mixtral-8x7B-Instruct-v0.1", # model_id from hf.co/models
+  'SM_NUM_GPUS': json.dumps(number_of_gpu), # Number of GPU used per replica
+  'MAX_INPUT_LENGTH': json.dumps(24000),  # Max length of input text
+  'MAX_BATCH_PREFILL_TOKENS': json.dumps(32000),  # Number of tokens for the prefill operation.
+  'MAX_TOTAL_TOKENS': json.dumps(32000),  # Max length of the generation (including input text)
+  'MAX_BATCH_TOTAL_TOKENS': json.dumps(512000),  # Limits the number of tokens that can be processed in parallel during the generation
+}
+
+# create hf model 
+llm_model = HuggingFaceModel(
+  role=role,
+  image_uri=llm_image,
+  env=config
+)
--- sage_init.py
+++ sage_init.py
@@ -22,3 +22,50 @@ sess = sagemaker.Session(default_bucket = sagemaker_session_bucket)
 print(f"sagemaker role arn: {role}")
 print(f"sagemaker session region: {sess.boto_region_name}")
 
+# # retrieve the llm image uri
+# llm_image = get_huggingface_llm_image_uri(
+#   "huggingface",
+#   version="1.3.1"
+# )
+
+region_mapping = {
+    "af-south-1": "626614931356",
+    "il-central-1": "780543022126",
+    "ap-east-1": "871362719292",
+    "ap-northeast-1": "763104351884",
+    "ap-northeast-2": "763104351884",
+    "ap-northeast-3": "364406365360",
+    "ap-south-1": "763104351884",
+    "ap-south-2": "772153158452",
+    "ap-southeast-1": "763104351884",
+    "ap-southeast-2": "763104351884",
+    "ap-southeast-3": "907027046896",
+    "ap-southeast-4": "457447274322",
+    "ca-central-1": "763104351884",
+    "cn-north-1": "727897471807",
+    "cn-northwest-1": "727897471807",
+    "eu-central-1": "763104351884",
+    "eu-central-2": "380420809688",
+    "eu-north-1": "763104351884",
+    "eu-west-1": "763104351884",
+    "eu-west-2": "763104351884",
+    "eu-west-3": "763104351884",
+    "eu-south-1": "692866216735",
+    "eu-south-2": "503227376785",
+    "me-south-1": "217643126080",
+    "me-central-1": "914824155844",
+    "sa-east-1": "763104351884",
+    "us-east-1": "763104351884",
+    "us-east-2": "763104351884",
+    "us-gov-east-1": "446045086412",
+    "us-gov-west-1": "442386744353",
+    "us-iso-east-1": "886529160074",
+    "us-isob-east-1": "094389454867",
+    "us-west-1": "763104351884",
+    "us-west-2": "763104351884",
+}
+
+llm_image = f"{region_mapping[sess.boto_region_name]}.dkr.ecr.{sess.boto_region_name}.amazonaws.com/huggingface-pytorch-tgi-inference:2.1.1-tgi1.3.1-gpu-py310-cu121-ubuntu20.04-v1.0"
+
+# print ecr image uri
+print(f"llm image uri: {llm_image}")
